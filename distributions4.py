fc6d8b8e6dd2b8baa982e3fe1a887d7cf0253b8e
Morrighan89 <riccardo.ferrero.math@gmail.com>
2017-05-02 19:11:09 +0200

    chupacabra

--- distributions4.py
+++ distributions4.py
@@ -240,7 +240,6 @@ class mapsHystEnergy:
         self.dist=DistCollector(mainDir)
     def integra(self,x,y):
         self.fullHyst=x[-1]-x[0]
-        print(self.fullHyst)
         if self.fullHyst==0:
            middle=int(np.round(x.size/4))
            top=int(np.round(x.size/2))
@@ -249,7 +248,6 @@ class mapsHystEnergy:
            self.result=-self._branchdown-self._branchup
         else:
            middle=int(np.round(x.size/2))
-           print(middle)
            self._branchup=integrate.simps(y[0: middle],x[0: middle])
            self._branchdown=integrate.simps(y[middle:x.size],x[middle:x.size])
            self.result=-self._branchdown-self._branchup
